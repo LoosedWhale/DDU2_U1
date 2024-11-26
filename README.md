be50044a3f8cabccb15b6609d92396b79b987437
Theodor <81869656+LoosedWhale@users.noreply.github.com>
2024-11-26 08:15:07 +0100

    Removed Comments and updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DDU2_U1
+
+http://webshare.mah.se/aq7153/
+
+https://github.com/LoosedWhale/DDU2_U1
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Recommended: All functions declared here
-
 function createCityBoxes() {
     for (let city of cities) {
         let cityBox = document.createElement("div");  
@@ -169,8 +167,6 @@ function createTable() {
     }
 }
 
-
-// Recommended: constants with references to existing HTML-elements
 const citiesDiv = document.querySelector("#cities"); 
 const h2 = document.querySelector("h2");
 const h3 = document.querySelector("h3");
@@ -179,7 +175,6 @@ const furthestCity = document.querySelector("#furthest");
 const table = document.querySelector("#table");
 const tabName = document.head.querySelector("title"); 
 
-// Recommended: Ask for the city name and then the rest of the code
 
 
 createCityBoxes();
